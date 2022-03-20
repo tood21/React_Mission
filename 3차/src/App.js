5f82c9978710b23e10bc0d3670a56a20b1977e59
stoneboat <tood2111@gmail.com>
2022-03-20 12:16:56 +0900

    3차 미션

---
+++ 3차/src/App.js
@@ -0,0 +1,35 @@
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import "./App.css";
+import { Outlet, Route, Routes } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
+import MainPage from "./pages/MainPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
